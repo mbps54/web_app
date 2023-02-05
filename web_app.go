1bc175fb9e8227b9d01fcf651d6d3465ae733290
mbps54 <utkin@posteo.net>
2023-02-05 21:21:24 +0300

    Jenkinsfile_with_pipeline

--- web_app.go
+++ web_app.go
@@ -7,7 +7,7 @@ import (
 
 func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprint(w, "Hello, CI/CD!")
+		fmt.Fprint(w, "Hello, CI/CD!!")
 	})
 	err := http.ListenAndServe(":8081", nil)
 	if err != nil {
