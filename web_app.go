180b4e64ce9f3d2a994b2ce36d78ae896b5ecbb5
mbps54 <utkin@posteo.net>
2023-02-05 20:01:19 +0300

    web app

---
+++ web_app.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func main() {
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprint(w, "Hello, CI/CD!")
+	})
+	err := http.ListenAndServe(":8081", nil)
+	if err != nil {
+		panic(err)
+	}
+}
